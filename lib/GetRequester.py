ecaaeec1adf0337298c2f19d77a744dcedfab72d
Felix Omondi <136954351+Felix-svg@users.noreply.github.com>
2024-04-05 21:38:45 +0300

    Completed assignment

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,18 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        # URL = 'https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json'
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        # URL = 'https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json'
+        response = requests.get(self.url)
+        return response.json()
+
+
+
+response_body = GetRequester('https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json').get_response_body()
+print(response_body)
+json_object = GetRequester('https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json').load_json()
+print(json_object)
